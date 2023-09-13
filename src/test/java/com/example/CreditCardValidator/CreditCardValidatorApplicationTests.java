591deda227b4f7c7975836410fcf443901127ad0
TheFrisb <thefrisb@gmail.com>
2023-09-13 16:13:20 +0200

    init

---
+++ src/test/java/com/example/CreditCardValidator/CreditCardValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CreditCardValidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
